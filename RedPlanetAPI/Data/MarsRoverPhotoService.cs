14340100d3f9171c0849fb48e925e2004eace0f1
Andrew Smith <andrewcs149@gmail.com>
2021-11-13 00:01:40 -0800

    Fix manifest response read in issues.

--- RedPlanetAPI/Data/MarsRoverPhotoService.cs
+++ RedPlanetAPI/Data/MarsRoverPhotoService.cs
@@ -10,6 +10,14 @@ namespace RedPlanetAPI.Data
             _config = config;
         }
 
+        public async Task<MarsRoverManifest.Response> GetMarsRoverManifest(string rover)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/?manifests&api_key={_config["NASA_API_KEY"]}";
+            HttpResponseMessage HTTPresponse = await client.GetAsync(url);
+
+            return HTTPresponse.IsSuccessStatusCode ? await HTTPresponse.Content.ReadFromJsonAsync<MarsRoverManifest.Response>() : null;
+        }
         public async Task<MarsRoverPhoto.Response> GetRoverPhotos(MarsRoverPhoto.Params urlParams)
         {
             HttpClient client = new HttpClient();
--- RedPlanetAPI/Interfaces/IMarsRoverPhoto.cs
+++ RedPlanetAPI/Interfaces/IMarsRoverPhoto.cs
@@ -5,5 +5,6 @@ namespace RedPlanetAPI.Interfaces
     public interface IMarsRoverPhoto
     {
         Task<MarsRoverPhoto.Response> GetRoverPhotos(MarsRoverPhoto.Params? urlParams);
+        Task<MarsRoverManifest.Response> GetMarsRoverManifest(string rover);
     }
 }
