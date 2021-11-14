83a24b97d8abce2f12d72949ee188920f0bd6cf7
Andrew Smith <andrewcs149@gmail.com>
2021-11-14 14:33:21 -0800

    Add manifest component.

--- RedPlanetAPI/wwwroot/js/interop.js
+++ RedPlanetAPI/wwwroot/js/interop.js
@@ -1,9 +1,12 @@
 ﻿function toggleManifestVisiblity() {
     let element = document.getElementById("manifest");
+    let button = document.getElementById("toggleManifest");
 
     if (element.style.display === "flex" || element.style.display == "") {
         element.style.display = "none";
+        button.innerHTML = "Show Manfiest";
     } else {
         element.style.display = "flex";
+        button.innerHTML = "Hide Manifest";
     }
 }
